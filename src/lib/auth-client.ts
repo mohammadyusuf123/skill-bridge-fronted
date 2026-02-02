2b85f00eb97792fa1c2f7ffe589538e58ea33f00
Mohammad Yusuf <yusue786@gmail.com>
2026-02-02 13:01:58 +0600

    update auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: 'https://skill-bridge-backend-sooty.vercel.app/api/auth',
     fetchOptions: {
     credentials: 'include', // <-- important!
   },
