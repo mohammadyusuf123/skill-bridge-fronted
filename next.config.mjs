8d8470636d881eecfd88457521db731ef6bead2b
Mohammad Yusuf <yusue786@gmail.com>
2026-02-02 10:03:03 +0600

    adding rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/auth/:path*',
+        destination: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
