2856c9a32708f3c555894158140944098019342f
Mohammad Yusuf <yusue786@gmail.com>
2026-02-02 23:35:54 +0600

    fix dp issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/:path*`,
+      },
+    ];
+  },
 };
+
+
 
 export default nextConfig;
 
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,11 +1,12 @@
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: typeof window !== "undefined" ? window.location.origin : "",
   fetchOptions: {
-    credentials: 'include', // <-- important!
+    credentials: "include",
   },
 });
+
 
 export const {
   signIn,
