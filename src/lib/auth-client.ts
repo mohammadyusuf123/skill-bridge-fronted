3ccaac3c5ff28d1c8bc8dc5a5c179bb1adf422e8
Mohammad Yusuf <yusue786@gmail.com>
2026-02-03 15:41:52 +0600

    adding api/auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 import { usernameClient } from 'better-auth/client/plugins'
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+const API_URL = 'https://skill-bridge-backend-sooty.vercel.app/api/auth' 
 const APP_NAME = 'Skill Bridge'
 export const authClient = createAuthClient({
    baseURL: API_URL,
-  basePath: "/auth",
   baseURLType: 'server',
   plugins: [usernameClient()],
   // Enable smooth cookie handling
