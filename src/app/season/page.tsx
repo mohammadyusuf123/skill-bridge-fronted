ed650970ec88b91a3a8f928db503000f96f47612
Mohammad Yusuf <yusue786@gmail.com>
2026-02-03 19:29:05 +0600

    adding commit

--- src/app/season/page.tsx
+++ src/app/season/page.tsx
@@ -1,9 +1,37 @@
 import { userService } from '@/services/userSeason';
+import { cookies } from 'next/headers';
 import React from 'react'
 
 export default async function page() {
-   const { data: session} = await userService.getSession();
-  console.log('Session data:', session);
+ async function getSession() {
+    try {
+      const cookieStore = await cookies();
+
+      console.log(cookieStore.toString());
+
+      const res = await fetch(`https://skill-bridge-backend-sooty.vercel.app/api/auth/get-session`, {
+        headers: {
+          Cookie: cookieStore.toString(),
+        },
+        cache: "no-store",
+      });
+
+      const session = await res.json();
+
+      console.log(session);
+
+      if (session === null) {
+        return { data: null, error: { message: "Session is missing." } };
+      }
+
+      return { data: session, error: null };
+    } catch (err) {
+      console.error(err);
+      return { data: null, error: { message: "Something Went Wrong" } };
+    }
+  }
+  const session = await getSession();
+  console.log('Session in season page:', session);
   return (
     <div>This is from season</div>
   )
