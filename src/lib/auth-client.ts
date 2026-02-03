df446b87c31191e650171cbfcb7fab929fbecaf1
Mohammad Yusuf <yusue786@gmail.com>
2026-02-03 13:52:15 +0600

    update auth-client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,18 @@
-import { AppUser, UserRole, UserStatus } from "@/types";
 import { createAuthClient } from "better-auth/react";
-
+import { usernameClient } from 'better-auth/client/plugins'
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'
+const APP_NAME = 'Skill Bridge'
 export const authClient = createAuthClient({
-  baseURL: `${process.env.NEXT_PUBLIC_BETTER_AUTH_URL}/auth`,
+   baseURL: API_URL,
+  baseURLType: 'server',
+  plugins: [usernameClient()],
+  // Enable smooth cookie handling
   fetchOptions: {
-    credentials: "include",
+    credentials: 'include',
+  },
+  // Session fetching configuration
+  sessionFetchOptions: {
+    cache: 'no-store',
   },
 });
 
