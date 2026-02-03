4f701226167c0700b99af851545bf149c6bf0c81
Mohammad Yusuf <yusue786@gmail.com>
2026-02-03 22:29:39 +0600

    store cookies

--- src/app/season/page.tsx
+++ src/app/season/page.tsx
@@ -2,45 +2,46 @@ import { cookies } from 'next/headers';
 import React from 'react';
 
 export default async function Page() {
-  const getSession = async () => {
-    try {
-      const cookieStore = cookies();
-console.log(cookieStore.toString());
-      const res = await fetch(
-        'https://skill-bridge-backend-sooty.vercel.app/api/auth/get-session',
-        {
-          headers: {
-            Cookie: cookieStore.toString(),
-          },
-          cache: 'no-store',
-        }
-      );
+  const cookieStore = await cookies();
+//   const getSession = async () => {
+//     try {
+//       const cookieStore = cookies();
+// console.log(cookieStore.toString());
+//       const res = await fetch(
+//         'https://skill-bridge-backend-sooty.vercel.app/api/auth/get-session',
+//         {
+//           headers: {
+//             Cookie: cookieStore.toString(),
+//           },
+//           cache: 'no-store',
+//         }
+//       );
 
-      if (!res.ok) {
-        throw new Error('Failed to fetch session');
-      }
+//       if (!res.ok) {
+//         throw new Error('Failed to fetch session');
+//       }
 
-      const session = await res.json();
+//       const session = await res.json();
 
-      if (!session) {
-        return { data: null, error: { message: 'Session is missing.' } };
-      }
+//       if (!session) {
+//         return { data: null, error: { message: 'Session is missing.' } };
+//       }
 
-      return { data: session, error: null };
-    } catch (err) {
-      console.error(err);
-      return { data: null, error: { message: 'Something went wrong' } };
-    }
-  };
+//       return { data: session, error: null };
+//     } catch (err) {
+//       console.error(err);
+//       return { data: null, error: { message: 'Something went wrong' } };
+//     }
+//   };
 
-  // ✅ MUST await here
-  const session = await getSession();
+//   // ✅ MUST await here
+//   const session = await getSession();
 
-  console.log('Session in session page:', session);
+//   console.log('Session in session page:', session);
 
   return (
     <div>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <pre>{cookieStore.toString()}</pre>
       This is from session
     </div>
   );
