cb81cd398285941770cd62d0bc153fa5dbcfed6b
Mohammad Yusuf <yusue786@gmail.com>
2026-02-03 22:49:55 +0600

    chnage auth client

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/auth/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth/:path*`,
+      },
+    ];
+  },
 };
+
 
 
 
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,19 +2,26 @@ import { createAuthClient } from "better-auth/react";
 import { usernameClient } from 'better-auth/client/plugins'
 const API_URL = 'https://skill-bridge-backend-sooty.vercel.app/api/auth' 
 const APP_NAME = 'Skill Bridge'
+// export const authClient = createAuthClient({
+//    baseURL: API_URL,
+//   baseURLType: 'server',
+//   plugins: [usernameClient()],
+//   // Enable smooth cookie handling
+//   fetchOptions: {
+//     credentials: 'include',
+//   },
+//   // Session fetching configuration
+//   sessionFetchOptions: {
+//     cache: 'no-store',
+//   },
+// });
 export const authClient = createAuthClient({
-   baseURL: API_URL,
-  baseURLType: 'server',
-  plugins: [usernameClient()],
-  // Enable smooth cookie handling
+  baseURL: typeof window !== "undefined" ? window.location.origin : "",
   fetchOptions: {
-    credentials: 'include',
-  },
-  // Session fetching configuration
-  sessionFetchOptions: {
-    cache: 'no-store',
+    credentials: "include",
   },
 });
+
 
 export const {
   signIn,
