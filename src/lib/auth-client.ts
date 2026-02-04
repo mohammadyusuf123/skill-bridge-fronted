c8b65342cacecbab6541f8e70ae4ae07b74e0077
Mohammad Yusuf <yusue786@gmail.com>
2026-02-04 14:55:33 +0600

    change better auth url

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 import { usernameClient } from 'better-auth/client/plugins'
-const API_URL = 'https://skill-bridge-backend-sooty.vercel.app/api/auth' 
+const API_URL = 'https://skill-bridge-backend-production-27ac.up.railway.app/api/auth' 
 const APP_NAME = 'Skill Bridge'
 export const authClient = createAuthClient({
    baseURL: API_URL,
